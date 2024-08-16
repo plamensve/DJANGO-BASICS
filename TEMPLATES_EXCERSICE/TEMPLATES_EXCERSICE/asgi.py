e6baf4b7b6f80797cf2f7076cde295e562081bad
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2024-08-16 08:55:39 +0300

    TEST

---
+++ TEMPLATES_EXCERSICE/TEMPLATES_EXCERSICE/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for TEMPLATES_EXCERSICE project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TEMPLATES_EXCERSICE.settings')
+
+application = get_asgi_application()
