25bafebb7aa7934a23bc2b45c5ae1af2ec961089
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2024-08-21 20:09:48 +0300

    Petstagram - test project

--- petstagram/petstagram/pets/models.py
+++ petstagram/petstagram/pets/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+
+class Pet(models.Model):
+    name = models.CharField(
+        max_length=30,
+        null=False,
+        blank=False
+    )
+
+    pet_photo = models.URLField()
+
+    date_of_birth = models.DateField(
+        null=True,
+        blank=True
+    )
+
+    slug = models.SlugField(
+        unique=True,
+        null=False,
+        blank=True,
+        editable=False
+    )
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if not self.slug:
+            self.slug = slugify(f'{self.name}-{self.id}')
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
