0d8dfc85b96399f03a2dfc70477a7791d567dc11
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2024-09-02 19:13:26 +0300

    Class Base View - Exersice 02.09.2024

--- CBV_Exersice/CBV_Exersice/asgi.py
+++ CBV_Exersice/CBV_Exersice/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for CBV_Excersice project.
+ASGI config for CBV_Exersice project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CBV_Excersice.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CBV_Exersice.settings')
 
 application = get_asgi_application()
--- CBV_Exersice/CBV_Exersice/wsgi.py
+++ CBV_Exersice/CBV_Exersice/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for CBV_Excersice project.
+WSGI config for CBV_Exersice project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CBV_Excersice.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CBV_Exersice.settings')
 
 application = get_wsgi_application()
