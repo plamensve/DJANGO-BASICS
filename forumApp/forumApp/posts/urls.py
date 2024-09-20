cd23914b96c0d261d402c98e5fac12cd8379bf1f
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2024-09-20 17:00:00 +0300

    Forum App - urls, views, templates exersice

--- forumApp/forumApp/posts/urls.py
+++ forumApp/forumApp/posts/urls.py
@@ -1,3 +1,13 @@
-urlpatterns = [
+from django.urls import path
 
+from forumApp.posts.views import index, dashboard, games, books, notifications, settings, profile
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('dashboard/', dashboard, name='dashboard'),
+    path('games/', games, name='games'),
+    path('books/', books, name='books'),
+    path('notifications/', notifications, name='notifications'),
+    path('settings/', settings, name='settings'),
+    path('profile/', profile, name='profile')
 ]
--- forumApp/forumApp/posts/views.py
+++ forumApp/forumApp/posts/views.py
@@ -1,5 +1,31 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
+# Create your views here.
 def index(request):
-    return HttpResponse('Hello World')
+    return render(request, 'base.html')
+
+
+def dashboard(request):
+    return render(request, 'posts/bashboard.html')
+
+
+def games(request):
+    return render(request, 'games.html')
+
+
+def books(request):
+    return render(request, 'books.html')
+
+
+def notifications(request):
+    return render(request, 'notifications.html')
+
+
+def settings(request):
+    return render(request, 'settings.html')
+
+
+def profile(request):
+    return render(request, 'profile.html')
