9d60781aa2a049ee56025ad7cda3693ef9f0b179
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2024-10-09 20:00:47 +0300

    ADD: new models, new validator, admin panel

--- petstagram_2/petstagram_2/photos/validators.py
+++ petstagram_2/petstagram_2/photos/validators.py
@@ -5,3 +5,5 @@ def photo_size_validator(photo):
     limit = 5 * 1024 * 1024
     if photo.size > limit:
         raise ValidationError('The maximum file size that can be uploaded is 5MB')
+
+    #test
