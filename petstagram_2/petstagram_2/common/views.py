679e981fb3e7e3caebd117e5d784e924f1fb0c48
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2024-10-10 11:24:06 +0300

    ADD: like, comment, share functionality. ADD: template inheritance

--- petstagram_2/petstagram_2/common/views.py
+++ petstagram_2/petstagram_2/common/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from petstagram_2.common.models import Like
+from petstagram_2.photos.models import Photo
 
 
 def home_page(request):
-    return render(request, 'common/home-page.html')
+    all_photos = Photo.objects.all()
+
+    context = {
+        'all_photos': all_photos
+    }
+
+    return render(request, 'common/home-page.html', context)
+
+
+def like_functionality(request, photo_id):
+    photo = Photo.objects.get(id=photo_id)
+    liked_object = Like.objects.filter(to_photo=photo_id).first()
+
+    if liked_object:
+        liked_object.delete()
+    else:
+        like = Like(to_photo=photo)
+        like.save()
+
+    return redirect(request.META['HTTP_REFERER'] + f'#{photo_id}')
