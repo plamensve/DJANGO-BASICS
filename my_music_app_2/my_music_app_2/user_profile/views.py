2c91dd72c52f08d32dc49d0208d5a689765d0dd1
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2024-10-23 19:51:10 +0300

    ADD: ALL CRUD operations

--- my_music_app_2/my_music_app_2/user_profile/views.py
+++ my_music_app_2/my_music_app_2/user_profile/views.py
@@ -1,10 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView
+
+from my_music_app_2.album.models import Album
+from my_music_app_2.user_profile.models import Profile
 
 
 def profile_details(request):
-    return render(request, 'profile/profile-details.html')
+    profile = Profile.objects.first()
+    all_albums = Album.objects.all().count()
+
+    context = {
+        'profile': profile,
+        'all_albums': all_albums,
+    }
+
+    return render(request, 'profile/profile-details.html', context)
 
 
 def profile_delete(request):
-    return render(request, 'profile/profile-delete.html')
+    profile = Profile.objects.first()
+
+    if request.method == 'POST':
+        profile.delete()
+        return redirect('home-page')
+
+    context = {
+        'profile': profile,
+    }
+
+    return render(request, 'profile/profile-delete.html', context)
+
