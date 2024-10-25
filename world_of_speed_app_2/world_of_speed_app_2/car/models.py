8b40babdcfbfd21b5c414c730a1367baf81ba78f
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2024-10-25 11:38:05 +0300

    ADD: Car and User profile models

--- world_of_speed_app_2/world_of_speed_app_2/car/models.py
+++ world_of_speed_app_2/world_of_speed_app_2/car/models.py
@@ -1,3 +1,137 @@
+from django.core.validators import MaxLengthValidator, MinLengthValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+from world_of_speed_app_2.car.validators import car_year_validator
+from world_of_speed_app_2.user_profile.models import Profile
+
+
+class Car(models.Model):
+    CAR_TYPE_MAX_LENGTH = 10
+    CAR_MODEL_MIN_LENGTH = 1
+    CAR_MODEL_MAX_LENGTH = 15
+
+    class TypeChoices(models.TextChoices):
+        RALLY = 'Rally', 'Rally'
+        OPEN_WHEEL = 'Open-Wheel', 'Open-Wheel'
+        KART = 'Kart', 'Kart'
+        DRAG = 'Drag', 'Drag'
+        OTHER = 'Other', 'Other'
+
+    type = models.CharField(
+        null=False,
+        blank=False,
+        validators=[
+            MaxLengthValidator(CAR_TYPE_MAX_LENGTH),
+        ],
+        choices=TypeChoices.choices,
+    )
+
+    model = models.CharField(
+        null=False,
+        blank=False,
+        validators=[
+            MinLengthValidator(CAR_MODEL_MIN_LENGTH),
+            MaxLengthValidator(CAR_MODEL_MAX_LENGTH),
+        ],
+    )
+
+    year = models.IntegerField(
+        null=False,
+        blank=False,
+        validators=[
+            car_year_validator
+        ],
+    )
+
+    image_url = models.URLField(
+        null=False,
+        blank=False,
+        unique=True,
+        default="https://...",
+        error_messages={
+            'unique': "This image URL is already in use! Provide a new one."
+        }
+    )
+
+    price = models.FloatField(
+        null=False,
+        blank=False,
+        validators=(
+            MinValueValidator(1.0),
+        ),
+    )
+
+    owner = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name='owner',
+    )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- world_of_speed_app_2/world_of_speed_app_2/user_profile/models.py
+++ world_of_speed_app_2/world_of_speed_app_2/user_profile/models.py
@@ -1,3 +1,124 @@
+from django.core.validators import MinLengthValidator, MaxLengthValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+from world_of_speed_app_2.user_profile.validators import username_validator
+
+
+class Profile(models.Model):
+    USERNAME_MIN_LENGTH = 3
+    USERNAME_MAX_LENGTH = 15
+    AGE_MIN_VALUE = 21
+    PASSWORD_MAX_LENGTH = 20
+    FIRST_NAME_MAX_LENGTH = 25
+    LAST_NAME_MAX_LENGTH = 25
+
+    username = models.CharField(
+        null=False,
+        blank=False,
+        validators=[
+            MinLengthValidator(USERNAME_MIN_LENGTH),
+            MaxLengthValidator(USERNAME_MAX_LENGTH),
+            username_validator
+        ]
+    )
+
+    email = models.EmailField(
+        null=False,
+        blank=False
+    )
+
+    age = models.IntegerField(
+        null=False,
+        blank=False,
+        validators=[
+            MinValueValidator(AGE_MIN_VALUE)
+        ],
+        help_text="Age requirement: 21 years and above."
+    )
+
+    password = models.CharField(
+        null=False,
+        blank=False,
+        validators=[
+            MaxLengthValidator(PASSWORD_MAX_LENGTH)
+        ]
+    )
+
+    first_name = models.CharField(
+        null=True,
+        blank=True,
+        validators=[
+            MaxLengthValidator(FIRST_NAME_MAX_LENGTH)
+        ]
+    )
+
+    last_name = models.CharField(
+        null=True,
+        blank=True,
+        validators=[
+            MaxLengthValidator(LAST_NAME_MAX_LENGTH)
+        ]
+    )
+
+    profile_picture = models.URLField(
+        null=True,
+        blank=True,
+    )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
