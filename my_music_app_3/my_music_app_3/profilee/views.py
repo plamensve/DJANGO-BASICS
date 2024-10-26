bb47b7959688a6e872bcbbc94847f25e66be2c80
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2024-10-26 19:51:53 +0300

    EXAM PREP PROJECT: FINISHED

--- my_music_app_3/my_music_app_3/profilee/views.py
+++ my_music_app_3/my_music_app_3/profilee/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from my_music_app_3.profilee.models import Profile
 
 
 def profile_details(request):
-    return render(request, 'profile/profile-details.html')
+    profile = Profile.objects.first()
+    album_count = profile.album_set.count()
+
+    context = {
+        'profile': profile,
+        'album_count': album_count,
+    }
+    return render(request, 'profile/profile-details.html', context=context)
 
 
 def profile_delete(request):
+    profile = Profile.objects.first()
+
+    if request.method == 'POST':
+        profile.delete()
+        return redirect('index')
+
     return render(request, 'profile/profile-delete.html')
