5355143363893eaa814e3207dab95c55462bd268
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2024-10-27 09:37:28 +0200

    ADD: add 2 models

---
+++ django_basics_regular_exam/django_basics_regular_exam/posts/models.py
@@ -0,0 +1,68 @@
+from django.core.exceptions import ValidationError
+from django.core.validators import MinLengthValidator
+from django.db import models
+
+from django_basics_regular_exam.author.models import Author
+
+
+class Post(models.Model):
+    TITLE_MAX_LENGTH = 50
+    TITLE_MIN_LENGTH = 5
+    IMAGE_URL_HELP_TEXT = "Share your funniest furry photo URL!"
+
+    title = models.CharField(
+        null=False,
+        blank=False,
+        unique=True,
+        max_length=TITLE_MAX_LENGTH,
+        validators=[
+            MinLengthValidator(TITLE_MIN_LENGTH),
+        ]
+    )
+
+    image_url = models.URLField(
+        null=False,
+        blank=False,
+        help_text=IMAGE_URL_HELP_TEXT
+    )
+
+    content = models.TextField(
+        null=False,
+        blank=False,
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    author = models.ForeignKey(
+        Author,
+        on_delete=models.CASCADE,
+    )
+
+    # TODO: Трябва да тествам тази валидация
+    def clean(self):
+        if Post.objects.filter(title=self.title).exclude(id=self.id).exists():
+            raise ValidationError("Oops! That title is already taken. How about something fresh and fun?")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- django_basics_regular_exam/django_basics_regular_exam/settings.py
+++ django_basics_regular_exam/django_basics_regular_exam/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # My application
+    'django_basics_regular_exam.web',
+    'django_basics_regular_exam.author',
+    'django_basics_regular_exam.posts'
 ]
 
 MIDDLEWARE = [
@@ -121,6 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
